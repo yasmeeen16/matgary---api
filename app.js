d88054d3873e4594a4a5d4e87172b3040015606c
yasmeen shalaby <yasmeenshalaby16@gmail.com>
2019-02-17 07:23:40 +0200

    second v

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var server = express();
 require('dotenv').config();
 var path=require('path');
+var multer=require("multer");
 var mongoose = require('mongoose');
 
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/tourism';
@@ -16,11 +17,16 @@ server.set('views', path.join(__dirname, 'views'));
 server.set("view engine","ejs");
 server.set("views","./views");
 server.use(express.static(path.join(__dirname, 'public')));
+var uploadMid = multer({dest:"./public/imgs"});
 
 
-var userRouts = require("./controllers/user.js");
+var userRouts = require("./controller/user.js");
 server.use("/user",userRouts);
 server.use("/",userRouts);
+
+var RouteCategory = require('./controller/category');
+server.use('/category',RouteCategory);
+require('./Model/Category');
 
 server.listen(PORT,function(){
   console.log('server listen at port number ' + PORT);
