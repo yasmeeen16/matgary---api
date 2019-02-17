ca73e6e3fc2d19ce1f1cc82abdacb3ab3b78d9dc
yasmeen shalaby <yasmeenshalaby16@gmail.com>
2019-02-17 07:42:04 +0200

    second v4

--- controller/category.js
+++ controller/category.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var Router = express.Router();
+var BodyParser = require('body-parser');
+var expressValidato = require('express-validator');
+var BodyParserMid = BodyParser.urlencoded();//middle ware to get data from request body
+const path = require('path');
+const fs = require("fs");
+var mongoose = require("mongoose");
 
+require("../Model/Category");
 
+var categoryDataModel = mongoose.model("Category");
+Router.get('/allCategory',function(req,resp,next){
+
+    categoryDataModel.find({}, function(err, categories) {
+                      //resp.render("content/listCat.ejs",{  categories:  categories});
+                      resp.json({  categories:  categories});
+                  });
+
+});
 Router.get('/cat',function(req,resp,next){
 
         resp.json({msg:'cat'});
